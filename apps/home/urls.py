87a507021f399fde474a865516a4d4a85d821b84
Sergio Hurtado <serujiourutado@gmail.com>
2024-05-30 17:46:19 -0400

    adision y configuracion de seccion Años

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
 
     path('configuracion', views.configuracion, name='configuracion'),
     path('configuracion/periodos-academicos', views.crearPeriodoAcademico, name='crear_periodo'),
+    path('configuracion/años', views.crearAnios, name='crear_anios'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
